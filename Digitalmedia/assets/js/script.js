b4edf1e1fbe2cfcc78bba068a443175a8eb97a5e
RudyTchamba <rudyitiel559@gmail.com>
2025-09-02 08:40:32 +0100

    staring a new frontend project on a digitalmedia website

---
+++ Digitalmedia/assets/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for(let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    }
+}
